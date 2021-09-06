62eae38075e982f4e0195c3a54fd987a6dbfbf72
Junior <216026633@student.uj.ac.za>
2021-09-06 20:47:31 +0200

    Add header to home page

--- Server/server.js
+++ Server/server.js
@@ -1,7 +1,7 @@
 const express = require("express");
+const ejs = require("ejs");
 const app = express();
 const path = require("path");
-const ejs = require("ejs");
 const mongoose = require("mongoose");
 const dirname = __dirname.slice(0, __dirname.search(/Server/i) - 1);
 const dotenv = require("dotenv");
@@ -12,12 +12,12 @@ app.use(express.static(path.join(dirname)));
 dotenv.config({path: path.join(__dirname, '.env')});
 app.set('view engine', 'ejs');
 
-app.get('/', (res, req) => {
+app.get('/', (req, res) => {
 	res.render('index');
 });
 
 const PORT = process.env.PORT;
 
 app.listen(PORT, () => {
-	console.log(`Live at port:${PORT}`);
+	console.log(`Live at port ${PORT}`);
 })
