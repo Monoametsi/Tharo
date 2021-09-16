2e46a9bf8aaa1f0b3f49951453bf8d4b4ab0f791
Junior <216026633@student.uj.ac.za>
2021-09-16 12:02:31 +0200

    Add about me section

--- JS/index.js
+++ JS/index.js
@@ -1,26 +1,40 @@
-let i = 0;
+let index = 0;
 
 let typeEffector = () => {
 	let typeElem = document.getElementById("intro-msg");
-	let typeMsg = `Hi there!! Lovely to meet you, my name is Tharo, and am looking for a place to call home, 
+	let typeMsg = `Hi there! Lovely to meet you, my name is Tharo, and am looking for a place to call home, 
 				  lets take a trip down this page so I that can tell you a little about myself.`;
 	
-	if(i < typeMsg.length){
-		typeElem.innerHTML += typeMsg.charAt(i);
+	if(index < typeMsg.length){
+		typeElem.innerHTML += typeMsg.charAt(index);
     }
 	
-    i++;
+    index++;
     setTimeout(typeEffector, 50);
 }
 
 let homeVid = document.getElementById("home-page-vid");
+let abtUsVid = document.getElementById("abt-us-page-vid");
 let vidTitle = document.getElementById("video-title-cont");
+let abtUsTitle = document.getElementById("about-us-video-title-cont");
+let nav = document.getElementById("nav-cont");
+let header = document.getElementById("header-cont");
 
 homeVid.onended = function(){
 	
 	if(homeVid.ended){
+		header.style.display = 'initial';
+		nav.style.display = 'flex';
 		vidTitle.style.display = 'flex';
-		setTimeout(typeEffector, 3000);
+		setTimeout(typeEffector, 2100);
+	}
+	
+}
+
+abtUsVid.onended = function(){
+	
+	if(abtUsVid.ended){
+		abtUsTitle.style.display = 'flex';
 	}
 	
 }
