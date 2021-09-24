7a871bc518a624883fd6a8c34c690be1ec847a15
Junior <216026633@student.uj.ac.za>
2021-09-24 21:50:33 +0200

    Add video player functionality

--- JS/index.js
+++ JS/index.js
@@ -38,3 +38,94 @@ abtUsVid.onended = function(){
 	}
 	
 } */
+
+const vidStarterCont = document.getElementById('start-vid-cont');
+
+const toggleFunct = (play_btn, pause_btn) => {
+	if(window.getComputedStyle(play_btn, null).display !== 'none'){
+		play_btn.style.display = 'none';
+		pause_btn.style.display = 'flex';
+		return true;
+	}else{
+		play_btn.style.display = 'flex';
+		pause_btn.style.display = 'none';
+		return false;
+	}
+}
+
+vidStarterCont.onclick = function(){
+	const video = this.parentElement.nextElementSibling;
+	const play_btn = this.children[0];
+	const pause_btn = this.children[1];
+	
+	if(toggleFunct(play_btn, pause_btn)){
+		video.play();
+	}else{
+		video.pause();
+	}
+	
+}
+
+const muteBtnCont = document.getElementById('volume-cont');
+
+muteBtnCont.onclick = function(){
+	const unMuteBtn = this.children[0];
+	const muteBtn = this.children[1];
+	const video = this.parentElement.parentElement.nextElementSibling;
+	
+	if(toggleFunct(unMuteBtn, muteBtn)){
+		video.muted = true;
+	}else{
+		video.muted = false;
+	}
+}
+
+const fullScreenCont = document.getElementById('full-screener-cont');
+let isScreenMax = false;
+const doc = document.documentElement;
+const body = document.body;
+
+const switchToFullScreen = () => {
+	isScreenMax = true;
+	doc.requestFullscreen();
+	//console.log(doc.requestFullscreen);
+}
+
+const minimizeScreen = () => {
+	isScreenMax = false;
+	document.exitFullscreen();
+	//console.log(document.exitFullscreen);
+}
+
+fullScreenCont.onclick = function(){
+	const controls = this.parentElement.parentElement;
+	const video = this.parentElement.parentElement.nextElementSibling;
+	
+	doc.classList.toggle('hide-overflow');
+	body.classList.toggle('hide-overflow');
+	controls.classList.toggle('full-screen-converter');
+	controls.classList.toggle('add-overlay');
+	video.classList.toggle('full-screen-converter');
+	fullScreenCont.classList.toggle('minimize-screen');
+	
+	if(isScreenMax){
+		minimizeScreen();
+	}else{
+		switchToFullScreen();
+	}
+}
+
+window.onkeydown = (event) => {
+	const controls =  document.getElementById('controls-cont');
+	const video =  document.getElementById('video-cont');
+	console.log(doc.requestFullscreen);
+	if(event.keyCode === 122 || event.keyCode === 27){
+		isScreenMax = false;
+		doc.classList.remove('hide-overflow');
+		body.classList.remove('hide-overflow');
+		controls.classList.remove('full-screen-converter');
+		controls.classList.remove('add-overlay');
+		video.classList.remove('full-screen-converter');
+		fullScreenCont.classList.remove('minimize-screen');
+	}
+}
